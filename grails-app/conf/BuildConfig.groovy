b0d81f17af4a3da5829580f121e4c3aa4ef0f9a2
gus <gus@energizedwork.com>
2011-03-08 09:04:52 +0000

    [gus] added release plugin supporting lib

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,22 @@
+import org.apache.ivy.core.settings.IvySettings
+import org.apache.ivy.plugins.resolver.IvyRepResolver
+
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+
+IvyRepResolver ewResolver = new IvyRepResolver()
+IvySettings settings = new IvySettings()
+settings.defaultLatestStrategy = settings.getLatestStrategy('latest-time')
+ewResolver.settings = settings
+ewResolver.changingPattern = '^.+-SNAPSHOT$'
+ewResolver.name = 'ew'
+ewResolver.ivyroot = 'http://repo.energizedwork.com/'
+ewResolver.ivypattern = '[organisation]/[module]-ivy-[revision].xml'
+ewResolver.artroot = 'http://repo.energizedwork.com/'
+ewResolver.artpattern = '[organisation]/[module]-[revision](-[classifier]).[ext]'
+ewResolver.checkmodified = true
+ewResolver.latest = 'latest-time'
 
 grails.project.dependency.resolution = {
 	inherits("global") {
@@ -8,14 +24,18 @@ grails.project.dependency.resolution = {
 	}
 	log "warn"
 	repositories {
-		grailsPlugins()
-		grailsHome()
-		grailsCentral()
+        resolver ewResolver
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+        mavenRepo 'http://repository.codehaus.org'
 	}
 	dependencies {
 		test("org.codehaus.groovy.modules.http-builder:http-builder:0.5.0-RC2") {
 			excludes "groovy", "xml-apis"
 		}
-
+        build 'com.energizedwork:release-plugin:0.1-SNAPSHOT'
 	}
 }
